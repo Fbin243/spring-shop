e74b5e8834a47c3892d255e6f994a5489ffd125f
Nguyễn Thanh Bình <ntbinh21@clc.fitus.edu.vn>
2024-06-20 16:52:15 +0700

    chore: render a html page for testing

--- src/main/java/com/fbin/springshop/controller/UserController.java
+++ src/main/java/com/fbin/springshop/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.fbin.springshop.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fbin.springshop.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -14,8 +14,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
